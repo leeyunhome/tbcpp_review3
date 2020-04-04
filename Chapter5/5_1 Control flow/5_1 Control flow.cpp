1224c47b11fa9e07a8e113acdeb552aa98822ab5
leeyunhome <leeyunhome@daum.net>
2020-04-04 20:30:19 +0900

    add 5.1 제어흐름 개요 Control flow

---
+++ Chapter5/5_1 Control flow/5_1 Control flow.cpp
@@ -0,0 +1,18 @@
+// 5_1 Control flow.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	std::cout << "I love you" << std::endl;
+
+	exit(0);
+
+	// ...
+	std::cout << "I love you, too" << std::endl;
+
+	return 0;
+}
+
