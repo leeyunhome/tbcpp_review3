1da75ccefa6d93b152c2ec517a733ae2be144d94
leeyunhome <leeyunhome@daum.net>
2020-04-07 22:58:01 +0900

    add 9_5 증감 연산자 오버로딩하기

---
+++ Chapter9/9_5_증감연사자오버로딩/9_5_증감연사자오버로딩.cpp
@@ -0,0 +1,53 @@
+﻿// 9_5_증감연사자오버로딩.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Digit
+{
+private:
+	int m_digit;
+
+public:
+	Digit(int digit = 0) : m_digit(digit) {}
+
+	// prefix
+	Digit& operator++()
+	{
+		++m_digit;
+		return *this;
+	}
+
+	// post
+	Digit operator++(int)
+	{
+		Digit temp{ m_digit };
+		++(*this);
+		return temp;
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const Digit& digit)
+	{
+		out << digit.m_digit;
+		return out;
+	}
+};
+
+int main()
+{
+	using namespace std;
+
+	Digit digit1{ 5 };
+
+	cout << ++digit1 << endl;
+	cout << digit1 << endl;
+
+	cout << "====postfix == =" << endl;
+
+	cout << digit1++ << endl;
+	cout << digit1 << endl;
+
+
+	return 0;
+}
+
