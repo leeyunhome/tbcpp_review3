84334ea158e2eb639c6a578827d8ac3f284ac63a
leeyunhome <leeyunhome@daum.net>
2020-04-17 22:09:09 +0900

    add 9.7 괄호연산자 오버로딩과 함수 객체

---
+++ Chapter9/9_7_괄호연산자오버로딩과함수객체/9_7_괄호연산자오버로딩과함수객체.cpp
@@ -0,0 +1,24 @@
+﻿// 9_7_괄호연산자오버로딩과함수객체.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Accumulator
+{
+private:
+    int m_counter = 0;
+
+public:
+    int operator()(int i) { return (m_counter += i); }
+};
+
+int main()
+{
+    Accumulator acc;
+    //cout << acc(10) << endl; // 이런 형태로 사용을 하는 것을 functor라고 부른다.
+    //cout << acc(20) << endl;
+    cout << acc(acc(acc(10))) << endl;
+    return 0;
+}
