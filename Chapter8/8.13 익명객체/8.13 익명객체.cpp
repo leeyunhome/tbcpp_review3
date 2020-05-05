2f017dd53cd1624f0804c1421582394a08292a20
leeyunhome <leeyunhome@daum.net>
2020-05-05 21:08:08 +0900

    8.13 익명 객체 R value처럼 작동을 한다.

---
+++ Chapter8/8.13 익명객체/8.13 익명객체.cpp
@@ -0,0 +1,53 @@
+﻿// 8.13 익명객체.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Cents
+{
+private:
+	int m_cents;
+
+public:
+	Cents(int cents) { m_cents = cents; }
+
+	int getCents() const { return m_cents; }
+};
+
+Cents add(const Cents& c1, const Cents& c2)
+{
+	return Cents(c1.getCents() + c2.getCents());
+}
+class A
+{
+public:
+	int m_value;
+	A(const int& value_in)
+	{
+		cout << "Constructor" << endl;
+		m_value = value_in;
+	}
+	~A()
+	{
+		cout << "Destructor" << endl;
+	}
+	void printDouble()
+	{
+		cout << m_value * 2 << endl;
+	}
+};
+int main()
+{
+	Cents c1(10);
+	Cents c2(20);
+	Cents c3 = add(c1, c2);
+	cout << c3.getCents() << endl;
+	//A a(1);
+	//a.printDouble();
+
+	//A(1).printDouble();
+
+	return 0;
+}
