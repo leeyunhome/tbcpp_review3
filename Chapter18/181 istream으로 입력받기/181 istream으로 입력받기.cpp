5e99c7071ce8a73828c9240b79120a7952bcb007
leeyunhome <leeyunhome@daum.net>
2020-07-03 23:30:29 +0900

    18.1 istream으로 입력받기

---
+++ Chapter18/181 istream으로 입력받기/181 istream으로 입력받기.cpp
@@ -0,0 +1,69 @@
+﻿// 181 istream으로 입력받기.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <iomanip> // input/output manipulators
+
+using namespace std;
+
+int main()
+{
+	char buf[1024];
+
+	//cout << (char)cin.peek() << endl;
+	//cin.ignore(2);
+
+	cin >> buf;
+	cout << buf << endl;
+
+	//cin.unget();
+	cin.putback('A');
+
+	cin >> buf;
+	cout << buf << endl;
+
+	/*int i;
+	float f;
+
+	cin >> i >> f;
+
+	cout << i << " " << f << endl;*/
+
+	//char ch;
+	//char buf[100];
+	/*string buf;
+
+	getline(cin, buf);
+	cout << cin.gcount() << " " << buf << endl;*/
+
+	/*cin.getline(buf, 100);
+	cout << cin.gcount() << " " << buf << endl;
+	
+	cin.getline(buf, 100);
+	cout << cin.gcount() << " " << buf << endl;*/
+
+	//while (cin >> ch)
+	
+
+	/*char buf[5];
+
+	cin >> setw(5) >> buf;
+	cout << buf << endl;
+
+	cin >> setw(5) >> buf;
+	cout << buf << endl;
+	
+	cin >> setw(5) >> buf;
+	cout << buf << endl;*/
+
+	//cout << "Enter a number" << endl;
+
+	//int i;
+	//cin >> i;
+
+	//cout << i << endl;
+
+	return 0;
+}
+
