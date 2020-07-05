198afef08ae0baca1ba1b268d54b9dd08b737f8e
leeyunhome <leeyunhome@daum.net>
2020-07-05 17:54:05 +0900

    add 2.3 C++ 11 고정 너비 정수 (Fixed-width Integers)

---
+++ Chapter2/2.3 c++ 11 Fixed-width Integers/2.3 c++ 11 Fixed-width Integers.cpp
@@ -0,0 +1,20 @@
+// 2.3 c++ 11 Fixed-width Integers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	std::int16_t i(5);
+	std::int8_t myint = 65;
+
+	cout << myint << endl;
+
+	std::int_fast8_t fi(5);
+	std::int_least64_t fl(5);
+
+	return 0;
+}
+
